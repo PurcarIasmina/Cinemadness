175b560be7b726ce2ef572095d440ce58272a5d7
Robert Pavaloni <robertpavaloni662000@yahoo.com>
2022-04-11 22:39:43 +0300

    unlogged user main page dummy setup

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import {Fragment} from 'react';
+
+import backgroundImage from '../../assets/backgroundImage.jpg';
+import classes from './Header.module.css';
+const Header = props => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>Logo</h1>
+            <h1>Searcb</h1>
+            <button>LogIn</button>
+            <button>SignUp</button>
+        </header>
+        <div className={classes['main-image']}>
+           <img src={backgroundImage} alt=""></img> 
+        </div>    
+    </Fragment>
+};
+
+export default Header;
