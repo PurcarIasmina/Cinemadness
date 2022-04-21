08d07fbd02bf428a88e4866a21238c77b45916ac
PurcarIasmina <purcar.iasminamaria@yahoo.com>
2022-04-21 14:37:22 +0300

    added routes and created register page

---
+++ src/components/Layout/Register.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Register()
+{
+    return(
+        <div className="Register">
+            <input type="text"></input>
+        </div>
+    );
+}
+export default Register;
